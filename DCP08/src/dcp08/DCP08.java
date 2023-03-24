5297de787e22300cf51c739def1f7e76a6d81b49
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-24 10:10:17 -0400

    initial commit & instructions added

---
+++ DCP08/src/dcp08/DCP08.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dcp08;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class DCP08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       /*
+       A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+       Given the root to a binary tree, count the number of unival subtrees.
+       For example, the following tree has 5 unival subtrees:
+
+           0
+          / \
+         1   0
+            / \
+           1   0
+          / \
+         1   1
+
+       */
+
+       
+    }
+    
+}
