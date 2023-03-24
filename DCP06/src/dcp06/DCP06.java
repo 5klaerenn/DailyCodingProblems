667d5378ee317f50cfac6b43e63d2b1da724a956
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-24 10:06:43 -0400

    added instructions

--- DCP06/src/dcp06/DCP06.java
+++ DCP06/src/dcp06/DCP06.java
@@ -14,7 +14,18 @@ public class DCP06 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       /*
+       An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next 
+        and prev fields, it holds a field named both, which is an XOR of the next node and the previous 
+        node. Implement an XOR linked list; it has an add(element) which adds the element to the end, 
+        and a get(index) which returns the node at index.
+       
+        If using a language that has no pointers (such as Python), you can assume you have access to 
+        get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
+       */
+        
+        
     }
     
 }
