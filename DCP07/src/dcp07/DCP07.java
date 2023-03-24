a4614a3321a23dc2e3fd28e8791e4918ee7485ac
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-24 10:08:40 -0400

    initial commit & instructions added

---
+++ DCP07/src/dcp07/DCP07.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dcp07;
+
+/**
+ *
+ * @author Sklaerenn
+ */
+public class DCP07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+        Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+        For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+        You can assume that the messages are decodable. For example, '001' is not allowed.
+        */
+       
+
+    }
+    
+}
