4cf0600a263e91f70a67e1562b325aabd7ed3aa3
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-24 09:55:38 -0400

    added instructions

--- DCP05/src/dcp05/DCP05.java
+++ DCP05/src/dcp05/DCP05.java
@@ -14,7 +14,21 @@ public class DCP05 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+       /*
+        cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that 
+        pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+        Given this implementation of cons:
+
+        def cons(a, b):
+            def pair(f):
+                return f(a, b)
+            return pair
+
+        Implement car and cdr.
+        */
+        
     }
     
 }
