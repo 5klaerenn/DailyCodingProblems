f6417be90c866c3e57eb7fc57e2d721fa17f3e09
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-24 09:52:08 -0400

    added instructions

--- DCP03/src/dcp03/DCP03.java
+++ DCP03/src/dcp03/DCP03.java
@@ -14,7 +14,27 @@ public class DCP03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+       /*
+       Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+       and deserialize(s), which deserializes the string back into the tree.
+
+        For example, given the following Node class
+
+        class Node:
+            def __init__(self, val, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+
+        The following test should pass:
+
+        node = Node('root', Node('left', Node('left.left')), Node('right'))
+        assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+        */
+        
+        
     }
     
 }
