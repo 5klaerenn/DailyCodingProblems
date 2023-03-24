aff461e345755dd8f8d7519b2147a19e7d66a67e
Sklaerenn <69436470+5klaerenn@users.noreply.github.com>
2023-03-24 09:54:05 -0400

    added instructions

--- DCP04/src/dcp04/DCP04.java
+++ DCP04/src/dcp04/DCP04.java
@@ -14,7 +14,15 @@ public class DCP04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       /*
+        Given an array of integers, find the first missing positive integer in linear time and constant space. 
+        In other words, find the lowest positive integer that does not exist in the array. 
+        The array can contain duplicates and negative numbers as well.
+
+        For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+        You can modify the input array in-place.
+        */
     }
     
 }
